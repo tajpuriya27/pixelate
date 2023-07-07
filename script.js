1cb549e8d772f17b897cb839642205d100b65737
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-07-07 08:53:16 +0545

    Alpha v1
    Application with click functionality is availableto run and use.
    - Need to lower the rectangle size(tds) in beta version

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// Your code here
+let choosenColor;
+let myTable = document.getElementsByTagName("table")[0];
+let boxMatrix = document.getElementsByTagName("td");
+
+document.getElementById("add-row").addEventListener("click", addRow);
+function addRow() {
+  const newRow = document.createElement("tr");
+  for (let i = 0; i < 10; i++) {
+    const newBox = document.createElement("td");
+    newRow.appendChild(newBox);
+  }
+  myTable.appendChild(newRow);
+  for (let i = 0; i < boxMatrix.length; i++) {
+    boxMatrix[i].addEventListener("click", clickFun);
+  }
+}
+
+// Adding colors
+let myOption = document.getElementsByTagName("select")[0];
+myOption.addEventListener("change", optFun);
+
+function optFun(e) {
+  choosenColor = e.target.value;
+}
+
+function clickFun(e) {
+  if (e.target.classList.contains(choosenColor)) {
+    e.target.classList.remove(choosenColor);
+  } else {
+    e.target.removeAttribute("class");
+    e.target.classList.add(choosenColor);
+  }
+}
